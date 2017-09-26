b3e55217055a7442486f70b1ada1ffb3cd2a485d
Volodymyr Shevchenko <volodymyr.shevchenko@corp.kismia.com>
2017-09-26 16:19:27 +0300

    Add models

--- model.go
+++ model.go
@@ -5,11 +5,40 @@ import (
 	"errors"
 )
 
-type dialog struct {
-	ID    int     `json:"id"`
-	string  `json:"name"`
-	Price float64 `json:"price"`
+type Dialog struct {
+	Id    		int     `json:"id"`
+	OwnerId 	int  	`json:"name"`
+	Status	 	int     `json:"status"`
 }
+
+type User struct {
+	Id 		int 	`json:"id"`
+	UserName 	string 	`json:"user_name"`
+	Password 	string  `json:"password"`
+	Token 		string	`json:"token"`
+
+}
+
+type DialogUser struct {
+	UserId 	   	int 	`json:"user_id"`
+	DialogId 	int 	`json:"dialog_id"`
+	LastMessageId	int	`json:"last_message_id"`
+	IsRead		int	`json:"is_read"`
+	LastReadDate	int	`json:"last_read_date"`
+	CountNewMessages int	`json:"count_new_messages"`
+
+}
+
+type Message struct {
+	Id 		int 	`json:"id"`
+	Text 		string	`json:"text"`
+	UserId 		int	`json:"user_id"`
+	Created		int	`json:"created"`
+	DialogId	int	`json:"dialog_id"`
+	DeletedForMe	int	`json:"deleted_for_me"`
+	DeletedForAll	int	`json:"deleted_for_all"`
+}
+
 
 func (p *dialog) getProduct(db *sql.DB) error {
 	return errors.New("Not implemented")
